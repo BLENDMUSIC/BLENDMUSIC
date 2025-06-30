629d1adbf708d01f396cf761d8953f409597193d
BLENDMUSIC <blendmusic01@gmail.com>
2025-06-30 13:21:24 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function alertePremium() {
+    alert("Ce contenu est réservé aux membres Premium. Veuillez vous abonner y accéder.")
+}
+
+function toggleMenu() {
+    const menu = document.getElementById("menu");
+    menu.style.display = menu.style.display === "block" ? "none" : "block";
+}
