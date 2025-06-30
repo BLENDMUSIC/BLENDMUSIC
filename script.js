e3c0e47c2f29ac1211eaab3efa5685332a5273a8
BLENDMUSIC <blendmusic01@gmail.com>
2025-06-30 15:56:17 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -4,5 +4,5 @@ function alertePremium() {
 
 function toggleMenu() {
     const menu = document.getElementById("menu");
-    menu.style.display = menu.style.display === "block" ? "none" : "block";
+    menu.classList.toggle("show");
 }
